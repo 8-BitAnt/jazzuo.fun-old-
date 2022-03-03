fc31287c45d887fdc8222d2955e03aec1fc7bd6f
Anthony <69584738+8-BitAnt@users.noreply.github.com>
2022-03-03 04:34:53 +0000

    -removed htaccess

--- apache2/start-apache.sh
+++
@@ -1,8 +0,0 @@
-if ! test -f "/home/runner/.apt/usr/sbin/apache2" > /dev/null; then install-pkg apache2; fi
-if ! test -f "/home/runner/.apt/usr/lib/apache2/modules/libphp7.2.so"; then install-pkg libapache2-mod-php7.2; fi
-source /home/runner/.apt/etc/apache2/envvars
-export APACHE_RUN_DIR=$(pwd)
-export APACHE_LOG_DIR=$(pwd)/apache2
-export APACHE_PID_FILE=$(pwd)/apache2/apache2.pid
-export APACHE_DOCUMENT_ROOT=$(pwd)
-/home/runner/.apt/usr/sbin/apache2 -f "$(pwd)/apache2/apache2.conf" -DFOREGROUND
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>Hello, World!</h1>";
-echo "<p>Hello, Apache World!</p>";
-echo "<p>Hello, PHP World!</p>";
-?>
